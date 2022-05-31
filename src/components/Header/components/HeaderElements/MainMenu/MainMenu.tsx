7a2645b89a8cc7527949d8d1566c149d2652d23b
tipaproger <lancer5610@gmail.com>
2022-05-31 21:18:12 +0300

    added menu

---
+++ src/components/Header/components/HeaderElements/MainMenu/MainMenu.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export const MainMenu: React.FC = () => {
+    const SubMenu = ['1111', '2222', '3333'];
+
+    const [isMouseOnMenu, setIsMouseOnMenu] = useState(false);
+
+    const menu: Array<JSX.Element> = [];
+
+    return <></>;
+};
